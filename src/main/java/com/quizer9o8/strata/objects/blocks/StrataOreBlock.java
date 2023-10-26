fdf0d9c5aaa4e23114d46b1a16144020a7d9954e
GamingCrew69 <85794256+GamingCrew69@users.noreply.github.com>
2023-10-27 01:36:18 +0200

    Updated to 1.18.2

--- src/main/java/com/quizer9o8/strata/objects/blocks/StrataOreBlock.java
+++ src/main/java/com/quizer9o8/strata/objects/blocks/StrataOreBlock.java
@@ -1,8 +1,6 @@
 package com.quizer9o8.strata.objects.blocks;
 
-import net.minecraft.block.OreBlock;
-import net.minecraft.util.math.MathHelper;
-
+import net.minecraft.world.level.block.OreBlock;
 import java.util.Random;
 
 public class StrataOreBlock extends OreBlock
